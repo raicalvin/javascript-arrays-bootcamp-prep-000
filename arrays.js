3413dd4845d5ac46636740c03e26f44aee8401a8
Calvin Simran Rai <raicalvin@gmail.com>
2018-07-20 04:26:09 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,7 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
 function addElementToBeginningOfArray(arr, elementToAdd) {
-  
+  return arr.unshift(elementToAdd);
 };
 
 function destructivelyAddElementToBeginningOfArray(arr, elementToAdd) {
